d0d0088161e9cb50ac7a99cd9e28a8e40bbd8971
mikhailtarasevich <tarasevichmikhail93@gmail.com>
2023-05-26 13:03:58 +0400

    initial commit.

---
+++ src/main/java/com/mikhail/tarasevich/socialmedia/RestApiSocialMediaApplication.java
@@ -0,0 +1,13 @@
+package com.mikhail.tarasevich.socialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiSocialMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApiSocialMediaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mikhail/tarasevich/socialmedia/RestApiSocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikhail.tarasevich.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiSocialMediaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
